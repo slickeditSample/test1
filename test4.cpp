a0b871e0d798c8130e0c8f834a3fe9823c36f70b
danwhenry <dhenry@slickedit.com>
2019-10-25 13:20:52 -0400

    add test4.cpp

--- test4.cpp
+++ test4.cpp
@@ -1,5 +1,5 @@
 void func4() {
-    //Added from TEMP/branch1
-    //Added from branch1
-    //Added from TEMP/branch1
+    //Added from TEMP/branch1 x
+    //Added from branch1      x
+    //Added from TEMP/branch1 x
 }
