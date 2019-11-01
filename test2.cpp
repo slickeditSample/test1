93371c4431625c51344269ffa3f71485313d9027
danwhenry <dhenry@slickedit.com>
2019-11-01 15:40:31 -0400

    Change comment

--- test2.cpp
+++ test2.cpp
@@ -7,10 +7,6 @@
 int main (int argc, char *argv[])
 {
     printf("main in\n");  // Change to 
-<<<<<<< HEAD
-    return(0);            // CONFLICT
-=======
     return(0);            // conflict
->>>>>>> 2e202c8a866fbca2baa44002a15433fef7da2c5e
 }
 
