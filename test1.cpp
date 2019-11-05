c9a2f3e02ad2f21e8a9f2a1e2a2c869828eba90b
Dan <dhenry@slickedit.com>
2019-11-05 10:19:36 -0500

    removed comments

--- test1.cpp
+++ test1.cpp
@@ -16,10 +16,10 @@
 
 int main(int argc, char *argv[])
 {
-    // change in masterxyz
-    // change in masterxyz
-    // change in masterxyz
-    // change in masterxyz
+    // change in master
+    // change in master
+    // change in master
+    // change in master
     printf("main in\n");
     return(0);
     // non-conflict
