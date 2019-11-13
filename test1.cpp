be14e97a0fb6e9f723b5c64c3f810f7e04a70405
Dan <dhenry@slickedit.com>
2019-11-13 10:04:25 -0500

    Comment for test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -17,9 +17,6 @@
 int main(int argc, char *argv[])
 {
     // change in master
-    // change in master
-    // change in master
-    // change in master
     printf("main in\n");
     return(0);
     // non-conflict
