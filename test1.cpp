9689a91fb5d9d78b7b2784532265a9e49fecaded
Dan <dhenry@slickedit.com>
2019-11-21 12:26:57 -0500

    remove comments

--- test1.cpp
+++ test1.cpp
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-// This is a comment added from test1 directory
-// This is a comment added from test1 directory 2:09pm
-// This is a comment added from worktree-testBranch1 directory 2:18pm
-// This is a comment added from worktree-testBranch1a directory 2:18pm
-
 int main (int argc, char *argv[])
 {
     // change in masterxyz
