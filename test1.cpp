39a92d1e901f657921410be6d6ad3e4d0f658ae5
Dan <dhenry@slickedit.com>
2020-01-09 15:15:37 -0500

    change from master2

--- test1.cpp
+++ test1.cpp
@@ -33,3 +33,4 @@ int main(int argc, char *argv[])
 // Commit from /src/vcTest/git/master2/test1.cpp
 // Commit from /src/vcTest/git/master2/test1.cpp
 // Commit from /src/vcTest/git/master2/test1.cpp
+// Commit from /src/vcTest/git/master2/test1.cpp
