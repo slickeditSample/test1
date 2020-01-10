5d75ee08bd05a1c940087feae5eb83f397148b61
Dan <dhenry@slickedit.com>
2020-01-10 12:27:05 -0500

    Remove stuff at top on windows

--- test1.cpp
+++ test1.cpp
@@ -10,14 +10,6 @@
 //clark2
 //clark3
 
-// Commit from f:\src\VCTests\git3\master\test1.cpp
-// Commit from f:\src\VCTests\git3\master\test1.cpp
-// Commit from /src/vcTest/git/master/test1.cpp
-// Commit from /src/vcTest/git/master/test1.cpp
-// Commit from f:\src\VCTests\git3\master\test1.cpp
-// Commit from f:\src\VCTests\git3\master\test1.cpp
-// Commit from f:\src\VCTests\git3\master\test1.cpp
-
 /* 
   change on master
 */
