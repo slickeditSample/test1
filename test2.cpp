f80066b1dda3833773ce26a46cf39dac1b3163fa
Dan <dhenry@slickedit.com>
2020-03-20 15:21:12 -0400

    Change in test.cpp

--- test2.cpp
+++ test2.cpp
@@ -1,16 +1,10 @@
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> ca55064bf383e23011f1c5b6661dfa2cd02af519
-
 // func1
 // Adding a comment to test if all branches a pulled
 // Adding a comment to test if all branches a pulled
 int func1() {
 }
-<<<<<<< HEAD
 #include <stdio.h>
 
 //newline
@@ -33,12 +27,6 @@ int main (int argc, char *argv[])
 // Change on master
 
 // Change on master
-=======
-=======
-=======
->>>>>>> master
-=======
->>>>>>> master
 #include <stdio.h>
 //newline
 // more
@@ -50,11 +38,3 @@ int main (int argc, char *argv[])
     printf("main in\n");  // Change to 
     return(0);            // conflict ######
 }
-
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> master
-=======
->>>>>>> master
-=======
->>>>>>> master
