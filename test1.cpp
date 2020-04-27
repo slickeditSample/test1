28d4e3b78397416dffc3f3c6efafc8487892c57f
Dan <dhenry@slickedit.com>
2020-04-27 14:00:39 -0400

    Remove all comments

--- test1.cpp
+++ test1.cpp
@@ -1,9 +1,3 @@
-// This is a comment added from test1 directory
-// This is a comment added from test1 directory 2:09pm
-// newline
-// more
-// This is the change that Dan made 2019/10/29
-// new xyz
 
 #include <stdio.h>
 
