381127667e57d3c34970919f5ce031a0d8585a0e
Dan Henry <dhenry@slickedit.com>
2022-03-08 14:14:56 -0500

    Change on master

--- test1.cpp
+++ test1.cpp
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 // This is a comment added from test1 directory
-// This is a comment added from test1 directory 2:09pm
+// This is a comment added from test1 directory 2:09pmxyz
 // This is a comment added from worktree-testBranch1 directory 2:18pm
 
 int main (int argc, char *argv[])
 {
+    // change in masterxyzxyz
+    // change in masterxyzxyzxyz
     // change in masterxyz
-    // change in masterxyz
-    // change in masterxyz
-    // change in masterxyz
+    // change in masterxyzxyz
     printf("main in\n");
     return(0);
     Added from xyz
