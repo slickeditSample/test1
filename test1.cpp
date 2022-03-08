d2b24a7dd389dd434a66a20b37da3fa782de183d
Dan Henry <dhenry@slickedit.com>
2022-03-08 09:20:31 -0500

    Change after merging from xyz

--- test1.cpp
+++ test1.cpp
@@ -1,55 +1,7 @@
-// Add comment
-// Add comment
-// This is a comment added from test1 directory
-// This is a comment added from test1 directory 2:09pm
-// newline
-// more
-// This is the change that Dan made 2019/10/29
-// new xyz
-// new
-// new
-//clark1
-//clark2
-//clark3
-//SHannon date
-
-/*
-  change on master
-*/
 #include <stdio.h>
-
-// Mod in branch 1
-// Mod in branch 1
-
-// Comment 1
-// Comment 2
-// Comment 3
-
-// Change from windows branch1
-// Change from windows branch1
 
 int main(int argc, char *argv[])
 {
-    // change in branch1
-    // change in branch1
-    // change in master
-    // change in master
     printf("main in\n");
-    // change in func
     return(0);
 }
-// This is a comment added from test1 directory
-// This is a comment added from test1 directory 2:09pm
-// newline
-// more
-// This is the change that Dan made 2019/10/29
-// new xyz
-// new
-// new
-//clark1
-//clark2
-//clark3
-
-/*
-  change on master
-*/
