1776ff591dea606ff3328ebef82987a1cad815ed
Dan Henry <dhenry@slickedit.com>
2022-05-12 14:41:53 -0400

    Test

--- test1.cpp
+++ test1.cpp
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
+
 // On test1
 
 //INSERTED 1537a
+//INSERTED 1537b
+//INSERTED 1537c
+//INSERTED 1537d
 
-
-
-int main(int argc, char *argv[])  {
-
-
+int main(int argc, char *argv[]) {
     // On test1
     printf("main in\n");
     //INSERT A
     //INSERT B
 
-    //INSERT Bxxx
-    //INSERT Y
-
-    // On test1 change
+    // On test1
     return (0);
 }
