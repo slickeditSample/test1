53a2a715fe8308b1031468831845724f8ecf6120
Dan Henry <dhenry@slickedit.com>
2022-06-09 13:00:55 -0400

    Test to see if coloring goes away

--- test1.cpp
+++ test1.cpp
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-
 // On test1
 
 //INSERTED 1537a
-//INSERTED 1537b
-//INSERTED 1537c
-//INSERTED 1537d
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])         {
+
     // On test1
     printf("main in\n");
     //INSERT A
+
     //INSERT B
 
-    // On test1
+
+    //INSERT B
+    //INSERT Y
+
+    // On test1 change
     return (0);
 }
