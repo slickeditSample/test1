5b528270565a5a60ce001d5fa749dfa8e1973954
Dan Henry <dhenry@slickedit.com>
2022-06-12 12:28:53 -0400

    See if file gets updated properly.

--- test1.cpp
+++ test1.cpp
@@ -9,14 +9,14 @@ int main(int argc, char *argv[])         {
     // On test1
     printf("This is a test\n");
 
-    //INSERT A
-    //INSERT B
-    //INSERT C
-    //INSERT D
+    //INSERT 0x001
+    //INSERT 0x002
+    //INSERT 0x004
+    //INSERT 0x008
+    //INSERT 0x010
 
 
-    int i = 5150;
-
+    int i = 5150   ;
 
     return 0;
 }
