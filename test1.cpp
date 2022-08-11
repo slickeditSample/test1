849fcf2992e6df24df0cfb4aa302bdcd521e0918
Dan Henry <dhenry@slickedit.com>
2022-08-11 11:26:53 -0400

    Add 100 functions to test live diff scroll markers that are too long.

--- test1.cpp
+++ test1.cpp
@@ -1,25 +1,421 @@
 #include <stdio.h>
-
 // On test1
-
 //INSERTED 1537a
 
-int main(int argc, char *argv[])         {
+
+static int func0()
+{
+}
+
+static int func1()
+{
+}
+
+static int func2()
+{
+}
+
+static int func3()
+{
+}
+
+static int func4()
+{
+}
+
+static int func5()
+{
+}
+
+static int func6()
+{
+}
+
+static int func7()
+{
+}
+
+static int func8()
+{
+}
+
+static int func9()
+{
+}
+
+static int func10()
+{
+}
+
+static int func11()
+{
+}
+
+static int func12()
+{
+}
+
+static int func13()
+{
+}
+
+static int func14()
+{
+}
+
+static int func15()
+{
+}
+
+static int func16()
+{
+}
+
+static int func17()
+{
+}
+
+static int func18()
+{
+}
+
+static int func19()
+{
+}
+
+static int func20()
+{
+}
+
+static int func21()
+{
+}
+
+static int func22()
+{
+}
+
+static int func23()
+{
+}
+
+static int func24()
+{
+}
+
+static int func25()
+{
+}
+
+static int func26()
+{
+}
+
+static int func27()
+{
+}
+
+static int func28()
+{
+}
+
+static int func29()
+{
+}
+
+static int func30()
+{
+}
+
+static int func31()
+{
+}
+
+static int func32()
+{
+}
+
+static int func33()
+{
+}
+
+static int func34()
+{
+}
+
+static int func35()
+{
+}
+
+static int func36()
+{
+}
+
+static int func37()
+{
+}
+
+static int func38()
+{
+}
+
+static int func39()
+{
+}
+
+static int func40()
+{
+}
+
+static int func41()
+{
+}
+
+static int func42()
+{
+}
+
+static int func43()
+{
+}
+
+static int func44()
+{
+}
+
+static int func45()
+{
+}
+
+static int func46()
+{
+}
+
+static int func47()
+{
+}
+
+static int func48()
+{
+}
+
+static int func49()
+{
+}
+
+static int func50()
+{
+}
+
+static int func51()
+{
+}
+
+static int func52()
+{
+}
+
+static int func53()
+{
+}
+
+static int func54()
+{
+}
+
+static int func55()
+{
+}
+
+static int func56()
+{
+}
+
+static int func57()
+{
+}
+
+static int func58()
+{
+}
+
+static int func59()
+{
+}
+
+static int func60()
+{
+}
+
+static int func61()
+{
+}
+
+static int func62()
+{
+}
+
+static int func63()
+{
+}
+
+static int func64()
+{
+}
+
+static int func65()
+{
+}
+
+static int func66()
+{
+}
+
+static int func67()
+{
+}
+
+static int func68()
+{
+}
+
+static int func69()
+{
+}
+
+static int func70()
+{
+}
+
+static int func71()
+{
+}
+
+static int func72()
+{
+}
+
+static int func73()
+{
+}
+
+static int func74()
+{
+}
+
+static int func75()
+{
+}
+
+static int func76()
+{
+}
+
+static int func77()
+{
+}
+
+static int func78()
+{
+}
+
+static int func79()
+{
+}
+
+static int func80()
+{
+}
+
+static int func81()
+{
+}
+
+static int func82()
+{
+}
+
+static int func83()
+{
+}
+
+static int func84()
+{
+}
+
+static int func85()
+{
+}
+
+static int func86()
+{
+}
+
+static int func87()
+{
+}
+
+static int func88()
+{
+}
+
+static int func89()
+{
+}
+
+static int func90()
+{
+}
+
+static int func91()
+{
+}
+
+static int func92()
+{
+}
+
+static int func93()
+{
+}
+
+static int func94()
+{
+}
+
+static int func95()
+{
+}
+
+static int func96()
+{
+}
+
+static int func97()
+{
+}
+
+static int func98()
+{
+}
+
+static int func99()
+{
+}
+
+
+int main(int argc, char *argv[]) {
 
     // On test1
-    printf("This is a test\n");
+    printf("This  is  a  test\n");
+    printf("This  is  a  test\n");
+    printf("This  is  a  test\n");
+    printf("This  is  a  test\n");
+    printf("This  is  a  test\n");
+    printf("This  is  a  test\n");
+    printf("This  is  a  test\n");
+    printf("This  is  a  test\n");
+       printf("This    is  a  test\n");
 
-    //INSERT 0x1
-    //INSERT 0x2
-    //INSERT 0x4
-
-
-    //INSERT 0x8
-
-    int i =5150;
-
-
-
-
-    return 0;
+    return 0; // Test
 }
